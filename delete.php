2b5f230a2472910018d90188365882e1845a3485
PrayasSirole <86904629+PrayasSirole@users.noreply.github.com>
2021-07-06 17:19:34 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php 
+require("inc/db.php");
+echo "@Prayas";
+
+if (isset($_GET['id'])) {
+    // Delete record
+    try {
+        // SQL Statement
+        $sql = 'DELETE FROM products WHERE id = :id LIMIT 1';
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":id", $_GET['id'], PDO::PARAM_INT);
+        $stmt->execute();
+        if ($stmt->rowCount()) {
+            header("Location: index.php?status=deleted");
+            exit();
+        }
+        header("Location: index.php?status=fail_delete");
+        exit();
+    } catch (Exception $e) {
+        echo "Error " . $e->getMessage();
+        exit();
+    }
+} else {
+    // Redirect to index.php
+    header("Location: index.php?status=fail_delete");
+    exit();
+}
